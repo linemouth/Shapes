5ee19b9ce13a7cdb853ac7c65f6098b2e16c078f
Collin Eddy <linemouth@gmail.com>
2023-06-17 11:06:56 -0700

    1: Hardcoded Shapes

---
+++ Shapes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare all shapes
+            float width1 = 2;
+            float height1 = 8;
+
+            float width2 = 3;
+            float height2 = 3;
+
+            // Print the details of each shape
+            Console.WriteLine("Shape 1");
+            Console.WriteLine($"    Area: {width1 * height1}");
+            Console.WriteLine($"    Perimeter: {2 * width1 + 2 * height1}");
+
+            Console.WriteLine("Shape 2");
+            Console.WriteLine($"    Area: {height2 * width2}");
+            Console.WriteLine($"    Perimeter: {2 * height2 + 2 * width2}");
+        }
+    }
+}
