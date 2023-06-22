bcf2c8085ee286164154cf6d4cea8591cb687e0e
Collin Eddy <linemouth@gmail.com>
2023-06-22 11:37:21 -0700

    10. Add More Shapes

--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -40,12 +40,35 @@ namespace Shapes
                 this.height = height;
             }
         }
+        class Square : Rectangle
+        {
+            public override string Name => "Square " + name;
+
+            protected float side;
+
+            public Square(string name, float side) : base(name, side, side) { }
+        }
+        class Circle : Shape
+        {
+            public override string Name => "Circle " + name;
+            public override float Area => (float)Math.PI * radius * radius;
+            public override float Perimeter => (float)Math.PI * 2 * radius;
+
+            protected float radius;
+
+            public Circle(string name, float radius) : base(name)
+            {
+                this.radius = radius;
+            }
+        }
         static void Main(string[] args)
         {
             // Declare all shapes
             List<Shape> shapes = new List<Shape> {
                 new Rectangle("1", 2, 8),
-                new Rectangle("2", 3, 3)
+                new Rectangle("2", 3, 3),
+                new Square("3", 5),
+                new Circle("4", 1)
             };
 
             // Print the details of each shape
