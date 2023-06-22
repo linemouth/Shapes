99c2fb18a66a43f94ae44a0c1c1fecf090500338
Collin Eddy <linemouth@gmail.com>
2023-06-22 11:37:04 -0700

    9. Make a Hierarchy

--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -5,33 +5,47 @@ namespace Shapes
 {
     internal class Program
     {
-        class Shape
+        abstract class Shape
         {
-            public float Area => width * height;
-            public float Perimeter => 2 * width + 2 * height;
-            public string name;
-            public float width;
-            public float height;
+            public abstract string Name { get; }
+            public abstract float Area { get; }
+            public abstract float Perimeter { get; }
 
-            public Shape(string name, float width, float height)
+            protected string name;
+
+            public Shape(string name)
             {
                 this.name = name;
-                this.width = width;
-                this.height = height;
             }
+
             public void Print()
             {
-                Console.WriteLine(name);
+                Console.WriteLine(Name);
                 Console.WriteLine($"    Area: {Area}");
                 Console.WriteLine($"    Perimeter: {Perimeter}");
+            }
+        }
+        class Rectangle : Shape
+        {
+            public override string Name => "Rectangle " + name;
+            public override float Area => width * height;
+            public override float Perimeter => 2 * width + 2 * height;
+
+            protected float width;
+            protected float height;
+
+            public Rectangle(string name, float width, float height) : base(name)
+            {
+                this.width = width;
+                this.height = height;
             }
         }
         static void Main(string[] args)
         {
             // Declare all shapes
             List<Shape> shapes = new List<Shape> {
-                new Shape("Shape 1", 2, 8),
-                new Shape("Shape 2", 3, 3)
+                new Rectangle("1", 2, 8),
+                new Rectangle("2", 3, 3)
             };
 
             // Print the details of each shape
